4829bea8e7413295330156a0bf7fd76bd4e8c0cb
EvgenyaHramova <evgenya.hramova@yandex.ru>
2022-11-12 15:56:49 +0400

    new folder Z38

--- Zadacha38/Program.cs
+++ Zadacha38/Program.cs
@@ -1,5 +1,37 @@
 ﻿/*Задайте массив вещественных чисел. Найдите разницу
- между максимальным и минимальным элементов массива.
+ между максимальным и минимальным элементом массива.
  [3 7 22 2 78] -> 76
-*/ 
-Console.WriteLine("Hello, World!");
+*/
+int[] array = new int[new Random().Next(2, 10)];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(-10, 11);
+    Console.Write(array[i] + " ");
+}
+Console.WriteLine();
+
+
+int Difference(int[] array)
+{
+    int min = array[0];
+    int max = 0;
+    int diff = 0;
+    for (int i = 1; i < array.Length; i++)
+    {
+        if (array[i] >= max)
+        {
+            max = array[i];
+        }
+        else if (array[i] <= min)
+        {
+            min = array[i];
+        }
+
+    }
+    Console.WriteLine($"Минимальное значение элемента массива: {min};");
+    Console.WriteLine($"Максимальное значение элемента массива: {max};");
+    diff = max - min;
+    return diff;
+}
+
+Console.WriteLine("Разница между максимальным и минимальным элементами равна:  " + Difference(array));
