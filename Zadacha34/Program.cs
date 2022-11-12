18e1e95dfe9dc95cc4550d3331be58c7876bf939
EvgenyaHramova <evgenya.hramova@yandex.ru>
2022-11-12 12:37:45 +0400

    new folder Z34

--- Zadacha34/Program.cs
+++ Zadacha34/Program.cs
@@ -2,5 +2,27 @@
 Напишите программу, которая покажет количество чётных чисел в массиве.
 [345, 897, 568, 234] -> 2
 
-*/ 
-Console.WriteLine("Hello, World!");
+*/
+int[] array = new int[new Random().Next(1, 10)];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(100, 1000);
+    Console.Write(array[i] + " ");
+}
+Console.WriteLine();
+
+
+int Chetnoe(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+Console.WriteLine(Chetnoe(array));
