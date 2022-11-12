4654b70ef4f698084f74db00fa23b75cf841e055
EvgenyaHramova <evgenya.hramova@yandex.ru>
2022-11-12 16:33:32 +0400

    new folder Z38

--- Zadacha38/Program.cs
+++ Zadacha38/Program.cs
@@ -2,20 +2,32 @@
  между максимальным и минимальным элементом массива.
  [3 7 22 2 78] -> 76
 */
+
+double[] array = new double[new Random().Next(2, 5)];
+Random rand = new Random();
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] =  rand.Next(-10, 11) + rand.NextDouble();
+    Console.Write(array[i] + " ");
+} 
+
+
+/*
 int[] array = new int[new Random().Next(2, 10)];
 for (int i = 0; i < array.Length; i++)
 {
     array[i] = new Random().Next(-10, 11);
     Console.Write(array[i] + " ");
 }
+*/
 Console.WriteLine();
 
 
-int Difference(int[] array)
+double Difference(double[] array)
 {
-    int min = array[0];
-    int max = 0;
-    int diff = 0;
+    double min = array[0];
+    double max = 0;
+    double diff = 0;
     for (int i = 1; i < array.Length; i++)
     {
         if (array[i] >= max)
