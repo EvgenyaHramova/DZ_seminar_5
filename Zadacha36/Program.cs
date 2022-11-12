4c96bc6a9dd7c041417f5fa4f6399e48df203877
EvgenyaHramova <evgenya.hramova@yandex.ru>
2022-11-12 12:46:31 +0400

    new folder Z36

--- Zadacha36/Program.cs
+++ Zadacha36/Program.cs
@@ -5,4 +5,26 @@
 [-4, -6, 89, 6] -> 0
 
 */ 
-Console.WriteLine("Hello, World!");
+int[] array = new int[new Random().Next(1, 10)];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(-100, 101);
+    Console.Write(array[i] + " ");
+}
+Console.WriteLine();
+
+
+int Summa(int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i % 2 == 1)
+        {
+            sum= sum + array[i];
+        }
+    }
+    return sum;
+}
+
+Console.WriteLine(Summa(array));
